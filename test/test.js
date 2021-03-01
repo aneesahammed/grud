c46c0dfcc2335800e19ed14ce30ba0d92db0dbe0
@aneesahammed <aneesahammed@gmail.com>
2021-03-01 19:38:21 +0000

    adds test file

---
+++ test/test.js
@@ -0,0 +1,9 @@
+describe("grud tests", () => {
+  describe("Create records", () => {});
+  describe("Read records", () => {});
+  describe("Query records", () => {});
+  describe("Update records", () => {});
+  describe("Query records to verify update", () => {});
+  describe("Delete records to verify update", () => {});
+  describe("Verify delete", () => {});
+});
